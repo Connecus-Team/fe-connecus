7e41e22d3fce0c73888428a47053d25f4980a5f2
Hoa Nguyen <thanhthanhhoa123@gmail.com>
2021-12-17 23:45:37 +0700

    Fix Document page

--- src/views/pages/blog/Article.jsx
+++ src/views/pages/blog/Article.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Footer from '../../../components/footer/Footer';
-import Header from '../../../components/header/Header';
-import SidebarBlog from '../../../components/sidebars/SidebarBlog';
-import useDocumentTitle from '../../../components/useDocumentTitle';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../../../components/footer/Footer'
+import Header from '../../../components/header/Header'
+import SidebarBlog from '../../../components/sidebars/SidebarBlog'
+import useDocumentTitle from '../../../components/useDocumentTitle'
 
 const Article = () => {
-  useDocumentTitle('Article');
+  useDocumentTitle('Article')
   return (
     <div>
       <Header />
-      <div className="container article_page pt-100">
+      <div className="container article_page pt-30">
         <div className="main row" id="main-content">
           <div className="col-lg-8">
             <div id="content">
@@ -56,40 +56,33 @@ const Article = () => {
                     <div className="snippet">
                       <p>
                         <strong>Handshake release assets</strong>
-                        &nbsp;validation metrics first mover advantage ownership
-                        prototype. Handshake scrum project learning curve
-                        termsheet buzz bandwidth alpha pivot analytics supply
-                        chain interaction design.&nbsp;
+                        &nbsp;validation metrics first mover advantage ownership prototype.
+                        Handshake scrum project learning curve termsheet buzz bandwidth alpha pivot
+                        analytics supply chain interaction design.&nbsp;
                         <mark>
-                          Niche market deployment metrics venture customer
-                          funding buyer handshake twitter stock
+                          Niche market deployment metrics venture customer funding buyer handshake
+                          twitter stock
                         </mark>
                         .&nbsp;Pitch analytics assets.
                       </p>
                       <p>
-                        Attending a trade show can be a very effective method of
-                        promoting your company and its products. And one of the
-                        most effective ways to optimize your trade show display
-                        and increase traffic to your booth is through the use of
-                        banner stands.
+                        Attending a trade show can be a very effective method of promoting your
+                        company and its products. And one of the most effective ways to optimize
+                        your trade show display and increase traffic to your booth is through the
+                        use of banner stands.
                       </p>
                       <figure>
-                        <figcaption>
-                          This is a caption on this photo for reference
-                        </figcaption>
+                        <figcaption>This is a caption on this photo for reference</figcaption>
                       </figure>
                     </div>
                     <h3>What is Ceus?</h3>
                     <p>
-                      Last month, my wife, Anne Doe, took me to Las Vegas
-                      because she had to go for a business convention. Needless
-                      to say, she writes for an guide to casinos and I hate
-                      gambling. But then, she likes it and this supports us too,
-                      so I went along without any hassle. At first I was
-                      depressed, but then as I asked around and looked around, I
-                      ended up having more fun in Las Vegas than I would have
-                      thought. And no. I did not enter a single casino while I
-                      was there.
+                      Last month, my wife, Anne Doe, took me to Las Vegas because she had to go for
+                      a business convention. Needless to say, she writes for an guide to casinos and
+                      I hate gambling. But then, she likes it and this supports us too, so I went
+                      along without any hassle. At first I was depressed, but then as I asked around
+                      and looked around, I ended up having more fun in Las Vegas than I would have
+                      thought. And no. I did not enter a single casino while I was there.
                     </p>
                     {/* <h3>Entertainment</h3>
                     <p>
@@ -111,39 +104,31 @@ const Article = () => {
                     </p> */}
                     <h3>How Ceus Work?</h3>
                     <p>
-                      One of the greatest things about Las Vegas, Reno and
-                      Atlantic City (but especially Las Vegas) is the number of
-                      shows that are available. You can get to watch top class
-                      comedians like Jay Leno, Jerry Seinfeld, Ray Romano, Tim
-                      Allen and even the likes of Bill Cosby and Chris Rock. If
-                      you are into music you can watch female singers like
-                      Celine Dion, Mariah Carey, Britney Spears, Christina
-                      Aguilera and Beyonc?, male performers like Phil Collins,
-                      Eric Clapton, Snoopy Dog and bands like Oasis and Bon
-                      Jovi. I could go on and on but the list is endless. If you
-                      are into magic you can watch magicians like David
-                      Copperfield do their thing meters from you. Whatever you
-                      like, you can find it here from Western music to oldies to
-                      Jazz, Rock, Heavy Mettle to Trance. All you have to do is
-                      look at the itenary offered during your visit.
+                      One of the greatest things about Las Vegas, Reno and Atlantic City (but
+                      especially Las Vegas) is the number of shows that are available. You can get
+                      to watch top class comedians like Jay Leno, Jerry Seinfeld, Ray Romano, Tim
+                      Allen and even the likes of Bill Cosby and Chris Rock. If you are into music
+                      you can watch female singers like Celine Dion, Mariah Carey, Britney Spears,
+                      Christina Aguilera and Beyonc?, male performers like Phil Collins, Eric
+                      Clapton, Snoopy Dog and bands like Oasis and Bon Jovi. I could go on and on
+                      but the list is endless. If you are into magic you can watch magicians like
+                      David Copperfield do their thing meters from you. Whatever you like, you can
+                      find it here from Western music to oldies to Jazz, Rock, Heavy Mettle to
+                      Trance. All you have to do is look at the itenary offered during your visit.
                     </p>
                     <h3>Why Ceus?</h3>
                     <p>
-                      One of the greatest things about Las Vegas, Reno and
-                      Atlantic City (but especially Las Vegas) is the number of
-                      shows that are available. You can get to watch top class
-                      comedians like Jay Leno, Jerry Seinfeld, Ray Romano, Tim
-                      Allen and even the likes of Bill Cosby and Chris Rock. If
-                      you are into music you can watch female singers like
-                      Celine Dion, Mariah Carey, Britney Spears, Christina
-                      Aguilera and Beyonc?, male performers like Phil Collins,
-                      Eric Clapton, Snoopy Dog and bands like Oasis and Bon
-                      Jovi. I could go on and on but the list is endless. If you
-                      are into magic you can watch magicians like David
-                      Copperfield do their thing meters from you. Whatever you
-                      like, you can find it here from Western music to oldies to
-                      Jazz, Rock, Heavy Mettle to Trance. All you have to do is
-                      look at the itenary offered during your visit.
+                      One of the greatest things about Las Vegas, Reno and Atlantic City (but
+                      especially Las Vegas) is the number of shows that are available. You can get
+                      to watch top class comedians like Jay Leno, Jerry Seinfeld, Ray Romano, Tim
+                      Allen and even the likes of Bill Cosby and Chris Rock. If you are into music
+                      you can watch female singers like Celine Dion, Mariah Carey, Britney Spears,
+                      Christina Aguilera and Beyonc?, male performers like Phil Collins, Eric
+                      Clapton, Snoopy Dog and bands like Oasis and Bon Jovi. I could go on and on
+                      but the list is endless. If you are into magic you can watch magicians like
+                      David Copperfield do their thing meters from you. Whatever you like, you can
+                      find it here from Western music to oldies to Jazz, Rock, Heavy Mettle to
+                      Trance. All you have to do is look at the itenary offered during your visit.
                     </p>
                   </div>
                 </div>
@@ -293,52 +278,6 @@ const Article = () => {
                     </li>
                   </ul>
                 </div> */}
-                
-                <h4 className="h3 mb-30">Leave a reply</h4>
-                <div className="reply">
-                  <form>
-                    <div className="form-row">
-                      <div className="col-sm">
-                        <div className="form-group">
-                          <input
-                            type="text"
-                            className="form-control"
-                            name="reply-name"
-                            placeholder="Your Name"
-                          />
-                        </div>
-                      </div>
-                      <div className="col-sm">
-                        <div className="form-group">
-                          <input
-                            type="email"
-                            className="form-control"
-                            name="reply-email"
-                            placeholder="Email Address"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <textarea
-                        name="reply-message"
-                        rows={8}
-                        className="form-control"
-                        placeholder="Your Reply"
-                        defaultValue={''}
-                      />
-                    </div>
-                    <div className="d-flex justify-content-end">
-                      <Link
-                        to="#"
-                        className="btn btn-primary sweep_letter sweep_top">
-                        <div className="inside_item">
-                          <span data-hover="Reply"> Reply </span>
-                        </div>
-                      </Link>
-                    </div>
-                  </form>
-                </div>
               </div>
             </div>
           </div>
@@ -349,7 +288,7 @@ const Article = () => {
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Article;
+export default Article
